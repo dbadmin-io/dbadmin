ce9fb963b7758a1cc2ed5ea36fb7ddf2855fe85d
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2024-08-25 15:15:09 +0530

    Added banner.txt

---
+++ src/test/java/org/dbadmin/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dbadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Loaded Successfully
+    }
+}
